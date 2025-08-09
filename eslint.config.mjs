b732643f31ffe2c3fca415e88f0f3447b6a352a0
noormohammadsiddiqui-io <70965960+noormohammadsiddiqui-io@users.noreply.github.com>
2025-08-09 21:53:04 +0530

    lets go

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      ".next/**/*",
+      "out/**/*"
+    ]
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-require-imports": "warn"
+    }
+  }
 ];
 
 export default eslintConfig;
